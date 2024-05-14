e673baefb572b1a5ab88054d566faf157070e87d
Lava Ahmed <lavaahmed915@gmail.com>
2024-05-14 18:37:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name){
+    return array.filter(function(driver){
+        return  driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(array, lett){
+    return array.filter(function(driver){
+        const letterLength = lett.length;
+        return driver.slice(0,letterLength).toLowerCase() === lett.toLowerCase();
+    })
+}
+
+function matchName(array, name) {
+   return array.filter((driver) => name === driver.name);
+    
+    }
